17e88572ebc0c0fcea8b5a3607261b6c24497afc
Anurag Bhanu <130905151+anuragbhanu@users.noreply.github.com>
2024-02-25 15:37:35 +0000

    Authorities Data

---
+++ src/assets/data/data.jsx
@@ -0,0 +1,79 @@
+import {
+  img_1,
+  img_2,
+  img_3,
+  bansi_vihar,
+  Bawarchi,
+  chauka,
+  Raj_darbar,
+  kosi,
+} from "./Images/index";
+
+const data = [
+  {
+    quality: "Good",
+    quantity: "5 kg",
+    name: "Kosi Hostel Mess",
+    address: "Ashok Rajpath , Patna",
+    img_src: kosi,
+    time: "1 hour ",
+  },
+  {
+    quality: "Average",
+    quantity: "8 kg",
+    name: "NITP Mess",
+    address: "Ashok Rajpath , Patna",
+    img_src: img_1,
+    time: "2 hour ",
+  },
+  {
+    quality: "Exellent",
+    quantity: "3 kg",
+    name: "Bansi Vihar Restaurant",
+    address: "Ashok Rajpath , Patna",
+    img_src: bansi_vihar,
+    time: "30 min ",
+  },
+  {
+    quality: "Good",
+    quantity: "5 kg",
+    name: " Bawarchi Restaurant",
+    address: "Mahendroo, Ashok Rajpath , Patna",
+    img_src: Bawarchi,
+    time: "2 hour ",
+  },
+  {
+    quality: "Execellent",
+    quantity: "2 kg",
+    name: "KFC",
+    address: "PNM Mall , Patna",
+    img_src: img_2,
+    time: "2 hour",
+  },
+  {
+    quality: "Poor",
+    quantity: "4 kg",
+    name: "The Family Resturant",
+    address: "Gandhi Maindan, Patna",
+    img_src: img_3,
+    time: "1 hour",
+  },
+  {
+    quality: "Average",
+    quantity: "5 kg",
+    name: "Bhojan Raat Bhar",
+    address: "Patna Sahib, Patna",
+    img_src: Raj_darbar,
+    time: "10 min",
+  },
+  {
+    quality: "Good",
+    quantity: "10 kg",
+    name: "Chauka Foods",
+    address: "Patna Sahib, Patna",
+    img_src: chauka,
+    time: "25 min",
+  },
+];
+
+export default data;
---
+++ src/assets/data/images/index.js
@@ -0,0 +1,19 @@
+import img_1 from "./1.jpg"
+import img_2 from "./2.jpg"
+import img_3 from "./3.jpg"
+import bansi_vihar from "./Bansi_Vihar.jpg"
+import Bawarchi from "./Bawarchi.jpg"
+import chauka from "./chauka.jpg"
+import Raj_darbar from "./Raj_darbar.jpg"
+import kosi from "./kosi_hostel.jpg"
+
+export default {
+    img_1,
+    img_2,
+    img_3,
+    bansi_vihar,
+    Bawarchi,
+    chauka,
+    Raj_darbar,
+    kosi
+}
